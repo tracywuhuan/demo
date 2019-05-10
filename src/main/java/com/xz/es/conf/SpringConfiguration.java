f9b084b7bdda022bf0c3d8e7d300e88090a76e75
wuhuan <wuhuan@107.152.24.197>
2019-05-10 16:51:33 +0800

    read elasticsearch server params from application.properties

--- src/main/java/com/xz/es/conf/SpringConfiguration.java
+++ src/main/java/com/xz/es/conf/SpringConfiguration.java
@@ -6,8 +6,10 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class SpringConfiguration {
@@ -19,18 +21,25 @@ public class SpringConfiguration {
      * @return the transport client
      * @throws UnknownHostException the unknown host exception
      */
+	@Value("${elasticsearch.address}")
+	private String address;
+	
+	@Value("${elasticsearch.port}")
+	private int port;
+	
     @Bean
     public TransportClient transportClient() throws UnknownHostException {
-    	String address = "10.0.0.7";
+    	//String address = "9.119.56.156";
         TransportClient client = new PreBuiltXPackTransportClient(Settings.builder()
                 .put("cluster.name", "my-application")
                 .put("xpack.security.user", "elastic:123456")
                 .put("xpack.security.transport.ssl.enabled",true)
+                .put("client.transport.sniff", true)
                 .put("xpack.security.transport.ssl.truststore.path", "certs/elastic-certificates.p12")
                 .put("xpack.security.transport.ssl.keystore.path", "certs/elastic-certificates.p12")
                 .put("xpack.security.transport.ssl.verification_mode", "certificate")
                 .build())
-                .addTransportAddress(new TransportAddress(InetAddress.getByName(address), 9300));
+                .addTransportAddress(new TransportAddress(InetAddress.getByName(address), port));
         return client;
     }
 }
