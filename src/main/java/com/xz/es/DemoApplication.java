04d9d4d8fc4f3bb761d436bd7e48481480aafcad
wuhuan <wuhuan@macbook-pro-2.cn.ibm.com>
2019-05-16 22:40:44 +0800

    enable https

--- src/main/java/com/xz/es/DemoApplication.java
+++ src/main/java/com/xz/es/DemoApplication.java
@@ -1,7 +1,15 @@
 package com.xz.es;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +17,44 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+    //如果没有使用默认值80  
+    @Value("${http.port:8080}")  
+    Integer httpPort;  
+  
+    //正常启用的https端口 
+    @Value("${server.port}")  
+    Integer httpsPort;  
 
+    @Bean
+    @ConditionalOnProperty(name="condition.http2https",havingValue="true", matchIfMissing=false)
+    public TomcatServletWebServerFactory servletContainer() {  
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {  
+            @Override  
+            protected void postProcessContext(Context context) {  
+                SecurityConstraint constraint = new SecurityConstraint();  
+                constraint.setUserConstraint("CONFIDENTIAL");  
+                SecurityCollection collection = new SecurityCollection();  
+                collection.addPattern("/*");  
+                constraint.addCollection(collection);  
+                context.addConstraint(constraint);  
+            }  
+        };  
+        tomcat.addAdditionalTomcatConnectors(httpConnector());  
+        return tomcat;  
+    }  
+    
+    @Bean  
+    @ConditionalOnProperty(name="condition.http2https",havingValue="true", matchIfMissing=false)  
+    public Connector httpConnector() {  
+        System.out.println("启用http转https协议，http端口："+this.httpPort+"，https端口："+this.httpsPort);  
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");  
+        connector.setScheme("http");  
+        //Connector监听的http的端口号  
+        connector.setPort(httpPort);  
+        connector.setSecure(false);  
+        //监听到http的端口号后转向到的https的端口号  
+        connector.setRedirectPort(httpsPort);  
+        return connector;  
+    }
 }
